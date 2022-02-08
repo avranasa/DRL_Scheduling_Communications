c00bfcc23040f4279eea47bbaacc652752a9af1c
avranasa <46421575+avranasa@users.noreply.github.com>
2022-02-08 17:11:53 +0100

    Update Main.py
    Placing the hyperParameters and environment variables for ease of use.

--- Main_Real_Traffic/Main.py
+++ Main_Real_Traffic/Main.py
@@ -7,142 +7,136 @@ import ExponentialRule
 import DeepScheduler
 from torch import cuda
 
-
+#~~~~~~~ METHOD/Algorithm ~~~~~~~~~  
+methodAvailable = ["Knapsack","ExpRule", "IntegerLPoracle",  "DeepScheduler"]
+MethodUsed = methodAvailable[1] #Define your method
+if MethodUsed in ["DeepScheduler"]:
+    LOAD_MODEL = False #Choose if you want to load the NN model. If true you will be asked to enter the name of the model
+else: 
+    LOAD_MODEL = None
 
 
 #~~~~~~HYPER-PARAMETERS OF ALGORITHMS ~~~~~~~~~
-def Define_HyperParameters(Method):
-    '''Here for each algorithm the necesssary hyperparameters can be chosen. In general:
+'''Here for each algorithm the necesssary hyperparameters can be chosen. In general:
 
-    -'N_parallel_env_test': Defines the number of independent experiments that are simultaneously run. The performance 
-                            is measured as the average over all those experiments.
-    -'delta': It is a hyper-parameter that the exponential rule uses. Even though 0.01 was used in the original paper here
-              for  3,5,10,15MHz  better to use 0.999 and for  20MHz 0.68. ()
-    -'OracleTimeSteps': Defines how many timesteps ahead the algorithm knows using an oracle the future traffic 
-                        characteristics. In paper  it is the horizon T.(If it is 0 then ILP coincides to 'Knapsack' solution)
-    
-    -'TD3': If true then a baseline that is built on the basis of TD3 algorithm is used.
-    -'MemorizedActions': How many actions of previous time steps to be stored and will be used in the state. For Full CSI
-                         (i.e. PROTOCOL['CSIestimation']=="Full") put it to 0. For Statistical CSI (better referred in the
-                         paper as no-CSI since only the baseline FrankWolfe has access to the traffic and channel statistics)
-                         then make it equal to Max_latency of the class with highest lateny.
-    -'TradeOff_between_lossesCritic': With 0 you minimize L2-loss (i.e. DDPG). With 1 you do distibutional (Distributional DDPG). 
-                                      If in (0,1), it will use both losses but it is not recommended.
-    -'TradeOff_reward_punish': In the code there is both positive reward (when a user is satisfied and negative (when its
-                               latency constraint is exceeded without getting the required data). This arguments balances 
-                               between the positive rewards and the negative approximately like: "a*PosRewards - NegRewards*(1-a)"
-                               In the paper only the case of a=1 is considered (ignoring negative rewards).
-    -'SyncProcess_act': If it is in (0,1) then it is the momentum with which the target pocily network is updated.  If it is
-                        integer then the target policy network is synced after that number of iterations. $m_{target} in the paper.
-    -'SyncProcess_crt': The same as 'SyncProcess_act' but for the target value network.
-    -'ExploreProbability': With that probability the actor will randomly (using noisy networks) distort its parameters which 
-                           will lead to a different action. In the paper it is '\sigma_{explore}'.
-    -'AfterSamplesToWrite_train': After that many training samples it will log some training statistics.       
-    -'AfterSamplesToTest': After 'AfterSamplesToTest'/'BatchSize' iterations the agent stops to be trained and tested. If not 
-                          interested in such procedure increase its value more than 'MaxNsamples'.
-    -'MaxIter'/'MaxNsamples': The training procedure will stop after either more than 'MaxNsamples' the agent has been trained
-                              or more than 'MaxIter' iterations passed.                         
+-'N_parallel_env_test': Defines the number of independent experiments that are simultaneously run. The performance 
+                        is measured as the average over all those experiments.
+-'delta': It is a hyper-parameter that the exponential rule uses. Even though 0.01 was used in the original paper here
+            for  3,5,10,15MHz  better to use 0.999 and for  20MHz 0.68. ()
+-'OracleTimeSteps': Defines how many timesteps ahead the algorithm knows using an oracle the future traffic 
+                    characteristics. In paper  it is the horizon T.(If it is 0 then ILP coincides to 'Knapsack' solution)
 
-    '''
-    if Method == 'Knapsack':
-        hyper_param = {
-            'N_parallel_env_test':  1
-        }
-    elif Method == 'ExpRule':
-        hyper_param = {
-            'N_parallel_env_test':  3,
-            'delta': 0.999
-        }
-    elif Method == "IntegerLPoracle":
-        hyper_param ={
-            'N_parallel_env_test':  2,
-            'OracleTimesteps': 3
-        }
-    elif Method == 'DeepScheduler':  
-        hyper_param = {
-            'device': "cpu", #not tested on GPU
-            #Architecture of NNs  
-            'TD3': False,
-            'N_Quantiles': 50,
-            'MemorizedActions': 0, 
+-'TD3': If true then a baseline that is built on the basis of TD3 algorithm is used.
+-'MemorizedActions': How many actions of previous time steps to be stored and will be used in the state. For Full CSI
+                        (i.e. PROTOCOL['CSIestimation']=="Full") put it to 0. For Statistical CSI (better referred in the
+                        paper as no-CSI since only the baseline FrankWolfe has access to the traffic and channel statistics)
+                        then make it equal to Max_latency of the class with highest lateny.
+-'TradeOff_between_lossesCritic': With 0 you minimize L2-loss (i.e. DDPG). With 1 you do distibutional (Distributional DDPG). 
+                                    If in (0,1), it will use both losses but it is not recommended.
+-'TradeOff_reward_punish': In the code there is both positive reward (when a user is satisfied and negative (when its
+                            latency constraint is exceeded without getting the required data). This arguments balances 
+                            between the positive rewards and the negative approximately like: "a*PosRewards - NegRewards*(1-a)"
+                            In the paper only the case of a=1 is considered (ignoring negative rewards).
+-'SyncProcess_act': If it is in (0,1) then it is the momentum with which the target pocily network is updated.  If it is
+                    integer then the target policy network is synced after that number of iterations. $m_{target} in the paper.
+-'SyncProcess_crt': The same as 'SyncProcess_act' but for the target value network.
+-'ExploreProbability': With that probability the actor will randomly (using noisy networks) distort its parameters which 
+                        will lead to a different action. In the paper it is '\sigma_{explore}'.
+-'AfterSamplesToWrite_train': After that many training samples it will log some training statistics.       
+-'AfterSamplesToTest': After 'AfterSamplesToTest'/'BatchSize' iterations the agent stops to be trained and tested. If not 
+                        interested in such procedure increase its value more than 'MaxNsamples'.
+-'MaxIter'/'MaxNsamples': The training procedure will stop after either more than 'MaxNsamples' the agent has been trained
+                            or more than 'MaxIter' iterations passed.                         
 
-            #Training hyperparameters
-            'TradeOff_between_lossesCritic': 1.0, 
-            'TradeOff_reward_punish':1.0,   
-            'Gamma': 0.95,    
-            'BatchSize': 32,
-            'CapacityPool': 5*1000, 
-            'LearningRate_Critic': 10e-4,   
-            'LearningRate_Actor' : 10e-4,    
-            'SyncProcess_act': 0.2e-2,  
-            'SyncProcess_crt': 0.2e-2,
-            'ExploreProbability':0.25, 
-            'N_parallel_env_train': 16,  
+'''
 
+if MethodUsed == 'Knapsack':
+    HYPER_PARAMETERS = {
+        'N_parallel_env_test':  1
+    }
+elif MethodUsed == 'ExpRule':
+    HYPER_PARAMETERS = {
+        'N_parallel_env_test':  3,
+        'delta': 0.999
+    }
+elif MethodUsed == "IntegerLPoracle":
+    HYPER_PARAMETERS = {
+        'N_parallel_env_test':  2,
+        'OracleTimesteps': 3
+    }
+elif MethodUsed == 'DeepScheduler':  
+    HYPER_PARAMETERS = {
+        'device': "cpu", #not tested on GPU
+        #Architecture of NNs  
+        'TD3': False,
+        'N_Quantiles': 50,
+        'MemorizedActions': 0, 
 
-            #Monitoring progress
-            'CreateWriter': True,       
-            'AfterSamplesToWrite_train': 10*1000,            
-            'AfterSamplesToTest': 20*1000,
-            'N_parallel_env_test': 10,   
+        #Training hyperparameters
+        'TradeOff_between_lossesCritic': 1.0, 
+        'TradeOff_reward_punish':1.0,   
+        'Gamma': 0.95,    
+        'BatchSize': 32,
+        'CapacityPool': 5*1000, 
+        'LearningRate_Critic': 10e-4,   
+        'LearningRate_Actor' : 10e-4,    
+        'SyncProcess_act': 0.2e-2,  
+        'SyncProcess_crt': 0.2e-2,
+        'ExploreProbability':0.25, 
+        'N_parallel_env_train': 16,  
 
 
-            #Termination   
-            'MaxIter': 1000*1000, 
-            'MaxNsamples': 20*1000*1000
-        }
-    return hyper_param        
+        #Monitoring progress
+        'CreateWriter': True,       
+        'AfterSamplesToWrite_train': 10*1000,            
+        'AfterSamplesToTest': 20*1000,
+        'N_parallel_env_test': 10,   
 
+
+        #Termination   
+        'MaxIter': 1000*1000, 
+        'MaxNsamples': 20*1000*1000
+    }    
 
 
 
 
-
-if __name__ == "__main__":
 #~~~~~ SYSTEM PARAMETERS ~~~~~~~
-    #PROTOCOL
-    PROTOCOL = {
-        'CSIestimation' : "Full", #Choice between "Full" or "Statistical"
-        'RetransProtocol' : "HARQ-type I", #For the moment the only choice 
-        'TimeSlot': 1e-3
-        }
+#PROTOCOL
+PROTOCOL = {
+    'CSIestimation' : "Full", #Choice between "Full" or "Statistical"
+    'RetransProtocol' : "HARQ-type I", #For the moment the only choice 
+    'TimeSlot': 1e-3
+    }
 
-    #CHANNEL
-    CHANNEL = {
-        'ChannelType': "ExpMarkovTimeCorr",
-        }
-
-
-    #TRAFFIC
-    TRAFFIC = {    
-        'ClassesInfo':          [{'Max_Latency': 5, 'Data':1000, 'Importance':1.0, 'Arrival_Prob': 0.2}, 
-                                {'Max_Latency': 25, 'Data':5*1000, 'Importance':1.0, 'Arrival_Prob': 0.3},],   
-        'Kusers': 100,
-        'ProbTypeUser':         {'bicycle':0.2, 'bus':0.2, 'car':0.2, 'foot':0.2, 'train':0.2, 'tram':0.0}  
-        }
-    p_null = 1-sum( [cl['Arrival_Prob'] for cl in TRAFFIC['ClassesInfo'] ])
-    assert p_null > 0, "In every time slot there must be some probability that now new arrival happens."
-    TRAFFIC['ClassesInfo'].append({'Max_Latency':1, 'Data':0, 'Importance':0, 'Arrival_Prob':p_null})
+#CHANNEL
+CHANNEL = {
+    'ChannelType': "ExpMarkovTimeCorr",
+    }
 
 
-    #RESOURCES
-    RESOURCES = {
-        'BW' : 3.0e6   ,#in Hz
-        'ResourceBlock': 0.2e6# Must be a divisor/factor of BW
-        }  
-
-#~~~~~~~ METHOD/Algorithm ~~~~~~~~~  
-    methodAvailable = ["Knapsack","ExpRule", "IntegerLPoracle",  "DeepScheduler"]
-    MethodUsed = methodAvailable[0] #Define your method
-    if MethodUsed in ["DeepScheduler"]:
-        LOAD_MODEL = False #Choose if you want to load the NN model. If true you will be asked to enter the name of the model
-    else: 
-        LOAD_MODEL = None
+#TRAFFIC
+TRAFFIC = {    
+    'ClassesInfo':          [{'Max_Latency': 5, 'Data':1000, 'Importance':1.0, 'Arrival_Prob': 0.2}, 
+                            {'Max_Latency': 25, 'Data':5*1000, 'Importance':1.0, 'Arrival_Prob': 0.3},],   
+    'Kusers': 100,
+    'ProbTypeUser':         {'bicycle':0.2, 'bus':0.2, 'car':0.2, 'foot':0.2, 'train':0.2, 'tram':0.0}  
+    }
+p_null = 1-sum( [cl['Arrival_Prob'] for cl in TRAFFIC['ClassesInfo'] ])
+assert p_null > 0, "In every time slot there must be some probability that now new arrival happens."
+TRAFFIC['ClassesInfo'].append({'Max_Latency':1, 'Data':0, 'Importance':0, 'Arrival_Prob':p_null})
 
 
-#~~~~~~ RUNNING THE ALGORITHMS ~~~~~~~~~       
+#RESOURCES
+RESOURCES = {
+    'BW' : 3.0e6   ,#in Hz
+    'ResourceBlock': 0.2e6# Must be a divisor/factor of BW
+    }  
+
+
+
+if __name__ == "__main__":    
     #~~~ Creating the test-environment & Printing general Info ~~~~
-    HYPER_PARAMETERS = Define_HyperParameters(MethodUsed)
     env_test  = env.Environment( PROTOCOL, CHANNEL, TRAFFIC, RESOURCES, HYPER_PARAMETERS, 'test')  
     
 
